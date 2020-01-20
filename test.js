742e7711c1bb3e0d318a4471d0fa1192c73ec979
amerabdou08 <60101620+amerabdou08@users.noreply.github.com>
2020-01-20 15:11:45 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+<center><div style="width: 336px; height: 280px; position: relative;">
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 3  ABOVE 2 IMAGES  -->
+   
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-8376649411244885"
+     data-ad-slot="5046665511"
+     data-ad-format="auto"
+     data-full-width-responsive="true"
+     data-page-url="https://coolmagie.info/8-ways-to-get-the-cheapest-car-insurance-rates-possible/"
+     ></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
